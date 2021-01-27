f1fdb361b430a4ea718c18d4dc6c233ca0fbd1d2
DavidViolet18 <davidviolet18@gmail.com>
2021-01-27 14:32:11 +0000

    Create angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    Bientot disponible ...
+    <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'davidviolet18';
+}
